fcd773a2649b4a5558642b33b1ae8d4cf31be612
Mary-Kiki <felishakinshan@gmail.com>
2024-08-12 05:35:41 -0700

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Print using for loop
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}

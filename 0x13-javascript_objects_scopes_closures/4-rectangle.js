be6c998790f6f5032ec16297bd23ae69da4fc5cd
Mary-Kiki <felishakinshan@gmail.com>
2024-08-29 17:32:27 -0700

    Task 5

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if invalid dimensions
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print () {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width > 0 && this.height > 0) {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  }
+
+  double () {
+    if (this.width > 0 && this.height > 0) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
